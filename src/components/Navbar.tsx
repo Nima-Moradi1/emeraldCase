75381b6edddb697eeb04bf793741975dd283df66
nima moradi <mnima8100@gmail.com>
2024-07-08 00:09:20 +0330

    some css changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = async () => {
       <MaxWidthWrapper>
         <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
           <Link href='/' className='flex z-40 font-semibold'>
-            case<span className='text-green-600'>cobra</span>
+            <span className='text-sm mt-[1.9px]'>emerald</span><span className='text-primary'>case</span>
           </Link>
 
           <div className='h-full flex items-center space-x-4'>
