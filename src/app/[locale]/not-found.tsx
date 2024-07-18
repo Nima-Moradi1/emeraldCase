13ef5c7a7334001be694994389730340c9aca27a
nima moradi <mnima8100@gmail.com>
2024-07-18 23:07:58 +0330

    implemented not-found and error-handling page

---
+++ src/app/[locale]/not-found.tsx
@@ -0,0 +1,32 @@
+import Phone from '@/components/Phone';
+import { useTranslations } from 'next-intl';
+import Link from 'next/link';
+
+export default function NotFound() {
+  const t = useTranslations('NotFound')
+   return ( 
+    <div className='flex items-center justify-center gap-20 bg-slate-100 dark:bg-background min-h-screen'>
+      <div className='hidden lg:block'>
+        <Phone className='w-60' imgSrc='/404.jpeg'/>
+      </div>
+        <div className="flex flex-col items-center justify-center gap-y-5
+     dark:bg-background text-center"> 
+    <h2 className="lg:hidden text-9xl font-bold text-gray-800 dark:text-gray-400">404</h2>
+    <h1 className="text-3xl md:text-7xl font-bold text-gray-800 dark:text-gray-400">
+      Page Not Found !
+      </h1> 
+    <p className="text-2xl text-gray-600 dark:text-gray-500 mt-4">
+      {t('title')}</p> 
+      <Link 
+      className="mt-6 px-6 py-3 bg-primary text-white rounded-lg
+       text-lg hover:text-primary hover:bg-zinc-700 dark:hover:bg-white transition-all duration-300"
+      href="/"> 
+     {t(
+      'button'
+     )}
+       </Link> 
+       </div> 
+    </div>
+   
+       );
+       }
