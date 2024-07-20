4cade19a47799782c8f3e977cc3fe8c22874a1ec
nima moradi <mnima8100@gmail.com>
2024-07-20 15:25:41 +0330

    added translation for darkMode modal

--- src/components/ui/darkModeToggle.tsx
+++ src/components/ui/darkModeToggle.tsx
@@ -3,7 +3,6 @@
 import * as React from "react"
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { useTheme } from "next-themes"
-
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
@@ -11,25 +10,26 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { useTranslations } from "next-intl"
 
 const DarkModeToggle = () => {
   const { setTheme } = useTheme()
-
+  const t = useTranslations('DarkToggle')
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild className="bg-transparent border border-zinc-300 dark:border-zinc-700">
         <Button variant="outline" size="icon">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">{t('toggle')}</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="bg-transparent mt-3">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+          {t('dark')}
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+          {t('light')}
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
